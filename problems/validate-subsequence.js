b8b53c55466ded32dce77d31753a3330a43e5c25
Germán <56662567+grmnmtz@users.noreply.github.com>
2022-04-13 00:34:40 -0500

    add isValidSubsequence problem

---
+++ problems/validate-subsequence.js
@@ -0,0 +1,26 @@
+/* 
+
+We want to traverse our array to check if the given subsequence is contained within it in the same order.
+
+We keep track of each subsequence index found in the array with the variable seqIdx.
+
+If a subsequence index is found, we sum + 1 to the seqIdx variable and we keep looping the array 
+while comparing the next value in it with the new seqIdx index because we need the subsequence in order.
+
+We return true if the subsequence is contained within the array and false if it isn't. 
+We break the loop early if we find our valid subsequence before traversing the entire array.
+
+Big O Notation: 
+
+O(n) time | O(1) space where n is the length of the array.
+
+*/
+
+function isValidSubsequence(array, sequence) {
+  let seqIdx = 0
+  for (let index = 0; index < array.length; index++) {
+    if (seqIdx === sequence.length) break
+    if (sequence[seqIdx] === array[index]) seqIdx += 1
+  }
+  return seqIdx == sequence.length
+}
